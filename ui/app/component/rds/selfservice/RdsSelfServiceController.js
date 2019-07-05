4a1ffaffff5c3efaaef4ce1d8170be4058392d27
Kirik0 <mattkkim02@gmail.com>
2019-07-05 17:03:08 -0400

    Fixed case where user does not have correct SDLC membership

--- ui/app/component/rds/selfservice/RdsSelfServiceController.js
+++ ui/app/component/rds/selfservice/RdsSelfServiceController.js
@@ -116,8 +116,13 @@ class RdsSelfServiceController extends GatekeeperSelfServiceController {
         angular.forEach(vm.forms.grantForm.selectedRoles, (v, k) => {
             vm.selectedItems.forEach( (item) => {
                 if (v) {
-                    let appSpecificApprovalThreshold = vm.global.userInfo.approvalThreshold[item.application]['appSpecificApprovalThresholds'][k.toUpperCase()][vm.forms.awsInstanceForm.selectedAccount.sdlc.toLowerCase()];
-                    values.push(appSpecificApprovalThreshold);
+                    let resource = vm.global.userInfo.roleMemberships;
+                    if(resource[item.application].roles.DEV && resource[item.application].roles.DEV.includes(vm.forms.awsInstanceForm.selectedAccount.sdlc.toUpperCase())) {
+                        let appSpecificApprovalThreshold = vm.global.userInfo.approvalThreshold[item.application]['appSpecificApprovalThresholds'][k.toUpperCase()][vm.forms.awsInstanceForm.selectedAccount.sdlc.toLowerCase()];
+                        values.push(appSpecificApprovalThreshold);
+                    } else {
+                        values.push(-1);
+                    }
                 }
             });
         });
